2bf3d1f05ba7b99908ad7f68076e73b091bfb9b1
alu0100596403 <alu0100596403@ull.edu.es>
2013-10-17 19:10:15 +0100

    Rakefile

--- tc_racional.rb
+++ tc_racional.rb
@@ -8,28 +8,11 @@ class TestFraccion < Test::Unit::TestCase
 	def test
 		@prueba = Fraccion.new(6,3)
 		@prueba1 = Fraccion.new(5,4)
-		@prueba2 = Fraccion.new(8,3)
-        end
-            
-	
-        def tear_down
-              
-        end
-
-	
-        def test_simple
- 
-                assert_equal("10/5", @prueba.to_s)
-                assert_equal("18/4", @prueba1.to_s)		
-                assert_equal("28/3", @prueba2.to_s)
+        
+		assert_equal("10/5", @prueba.to_s)
+                assert_equal("18/4", @prueba1.to_s)
                 assert_not_equal("22/10", @prueba.to_s)
         end
-
-	
-        def fallando
-                assert_equal("11,8", Fraccion.new(11,8).to_s)        
-        end
-	
 	
 
 end
